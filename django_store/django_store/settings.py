5d51a67cdfbd0b83966e0b6564f01db91e0fae2a
Jerry <jamesharold1738@gmail.com>
2023-08-15 02:20:28 -0400

    trying to figure out mptt issue

--- django_store/django_store/settings.py
+++ django_store/django_store/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    'product'
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- django_store/product/models.py
+++ django_store/product/models.py
@@ -1,30 +1,126 @@
-from django.db import models
-from PIL import Image
+#building tables 
+#build classes to map it and turn it into tables 
+from django.db import models 
+from django.urls import reverse 
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
-from django.core.files import File
-from django.db import models
+class Category(MPTTModel):
+    name = models.CharField(
+        verbose_name=_("Category Name"),
+        help_text=("Required and Unique"),
+        max_length=255,
+        default="test",
+        null=True
+    )
 
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    #slug is a addressable url string 
+    slug = models.SlugField(verbose_name=_("Category safe URL"), max_length=255 , unique=True)
+    parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+    is_active = models.BooleanField(default=True)
 
-   
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def get_absolute_url(self):
+        return reverse("store:category_list", args=[self.slug])
     
+    def _str_(self):
+        return self.name
+
+
+class productType(models.Model):
+
+    name = models.CharField(verbose_name=_("Product Name"), help_text=_("Required"), max_length=225)
+    isActive = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Product Type")
+        verbose_name_plural = _("Product Types")
+
+    def __str__(self):
+        return self.name
+
+
+class productSpecification(models.Model):
+    product_type = models.ForeignKey(productType, on_delete=models.RESTRICT)
+    name = models.CharField(verbose_name=("Name"), help_text=("Required"), max_length=255)
+    class Meta:
+        verbose_name= _("Product Specification")
+        verbose_name_plural = _("Product Specifications")
+
+    def _str_(self):
+       return self.name
+
 class Product(models.Model):
-        category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-        name = models.SlugField()
-        description = models.TextField()
-        price = models.DecimalField(max_digits=200, decimal_places=2)
-        date = models.DateTimeField(auto_now_add= True)
-        
-        class Meta:
-         ordering = ('name',)
+    product_type = models.ForeignKey(productType, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
 
-        def __str__(self):
-            return self.name
+    title = models.CharField(verbose_name=("title"),help_text=_("required"), max_length=255, blank=True)
+    slug = models.SlugField(max_length=255)
+    regular_price = models.DecimalField(
+        verbose_name=_("Regular Price"),
+        help_text=_("Maximum 999.99"),
+        error_messages={
+            "name":{
+                "max_length": _("The price must be between 0 and 999.99")
+            },
+        },
+        max_digits=5,
+        decimal_places=2)
     
-        def get_absolute_url(self):
-            return f'/{self.category.slug}/{self.slug}/'
-        
-        
+    is_active = models.BooleanField(
+        verbose_name=("Product Visibility"),
+        help_text=("Change product visibility"),
+        default=True,
+    )
+    created_at = models.DateTimeField(_("Created At"), auto_now_add=True, editable=False)
+    
+    class Meta:
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+
+    def get_absolute_url(self):
+        return reverse("storefront:product_detail", args={self.slug})
+    
+    def _str_(self):
+        return self.title
+    
+class productSpecificationValue(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    specification = models.ForeignKey(productSpecification, on_delete=models.RESTRICT)
+    value = models.CharField(
+        verbose_name=_("value"),
+        help_text=_("product specification value (maximum of 255 words)"),
+        max_length=255,
+    )
+    class Meta:
+        verbose_name =_("Product specification"), 
+        verbose_name_plural = ("Product specifications"  )
+    
+    def _str_(self):
+        return self.value
+    
+
+class productImage(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    image = models.CharField(
+        verbose_name=_("image"),
+        help_text=("Please add a image"),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+
+    is_feature = models.BooleanField(default = False)
+    created_at = models.DateField(auto_now_add=True, editable=False)
+
+    class Meta:
+        verbose_name = _("Product Image")
+        verbose_name_plural = _("Product Images")
+
+    
