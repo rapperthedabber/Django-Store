c5da25c1c1c63d75c1917eb70edce71e3ec28553
Jerry <jamesharold1738@gmail.com>
2023-08-15 23:29:41 -0400

    product model error

--- django_store/product/admin.py
+++ django_store/product/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
-from .models import Category,Product,productType
+from mptt.admin import MPTTModelAdmin
+from .models import Category,Product,productType, productSpecification, productImage,productSpecificationValue
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(productType)
+
+class ProductSpecificationInLine(admin.TabularInline):
+    model = productSpecification
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationInLine,
+    ]
+
+class productImageSpecification(admin.TabularInline):
+        model = productImage
+
+class productSpecificationValueInline(admin.TabularInline):
+     model = productSpecificationValue
+
+
+class ProductAdmin(admin.ModelAdmin):
+     inlines = [
+          productSpecificationValue,
+          productImageSpecification
+     ]
+  
